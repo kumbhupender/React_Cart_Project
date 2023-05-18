f5c39e8b3aade6658cfd119fcb0429bef24ba429
Bhupender <Bhupender@kumarbhupender8171.com>
2023-05-18 10:52:23 +0530

    Destructuring our props

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,6 +6,7 @@ class CartItem extends React.Component {
         console.log('this.props',this.props);
         //destructuring
         const {price , title , qty} = this.props.product;
+        const {product, onIncreaseQuantity , onDecreaseQuantity} = this.props;
         return (
 
             <div className='cart-item'>
@@ -22,9 +23,9 @@ class CartItem extends React.Component {
 
                     <div className='cart-item-actions'>
                     {/* Buttons */}
-                    <img alt="increase" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992651.png'  onClick={() => this.props.onIncreaseQuantity(this.props.product)} />
+                    <img alt="increase" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992651.png'  onClick={() => onIncreaseQuantity(product)} />
 
-                    <img alt="decrease" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992683.png' onClick={() => this.props.onDecreaseQuantity(this.props.product)} />
+                    <img alt="decrease" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992683.png' onClick={() => onDecreaseQuantity(product)} />
 
                     <img alt="delete" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1214/1214428.png' />
                 </div>
