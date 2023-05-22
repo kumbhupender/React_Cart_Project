98fc8617f7a2efca80c745f6d7e837bfe52455fb
Bhupender <Bhupender@kumarbhupender8171.com>
2023-05-22 19:10:15 +0530

    Adding a firebase database in our cart cloud

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import * as firebase from 'firebase'; 
+// import 'firebase/firestore';
+import firebase from "firebase/app";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjZGTFo5z7gXf38hmaAMhhg9cA05hWrWo",
+  authDomain: "cart-c4b55.firebaseapp.com",
+  projectId: "cart-c4b55",
+  storageBucket: "cart-c4b55.appspot.com",
+  messagingSenderId: "977469476313",
+  appId: "1:977469476313:web:c62f2fc13dd9457e1eddc8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
